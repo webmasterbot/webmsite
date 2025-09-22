7642f76ba43df0194d4df90a070c2946fdf6a6ac
Beck Freeman <71795010+beackers@users.noreply.github.com>
2025-09-21 20:50:40 -0500

    Added basic heartbeat function to server Server now calculates last time a POST request was sent to /heartbeat Next step: - Add this functionality to main.html and add cool-style panel that turns different colors - Add heartbeat function to bot, so it posts stuff and we hear the bot instead of me making POST requests via python.requests module - GET ALL THIS UPLOADED WOOO

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import tracemalloc, asyncio,  os, logging, requests
+from flask import Flask, render_template, request
+import tracemalloc, asyncio,  os, logging, requests, time
 
 logging.basicConfig(filename="log.txt", level=logging.INFO, filemode="w")
 logger = logging.getLogger(__name__)
@@ -7,13 +7,23 @@ logger.info("Log started")
 
 app = Flask(__name__)
 
+lastHeard = "Not heard from"
+
 @app.route('/')
 def main():
-	return render_template("main.html", botStatus="All good")
+	return render_template("main.html", lastHeard=time.time()-lastHeard)
 
 @app.route('/heartbeat', methods=['POST'])
 def heartbeat():
-	return
+	global lastHeard
+	lastHeard = time.time()
+	data = request.data
+	print(data.decode())
+	return 200
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeatGet():
+	return str(time.time()-lastHeard)
 
 if __name__ == "__main__":
 	app.run(port=5000)
